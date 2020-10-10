21c64dbdf547fb24269aeca96a6f23946941857d
amaldevm19 <amaldevm19@gmail.com>
2020-10-10 16:46:58 +0400

    updated react App and station components

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -9,9 +9,23 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Stations from './components/stations/Stations';
 import AboutPI from './components/about/AboutPI';
+import socketIOClient from "socket.io-client";
+const socket = socketIOClient("http://127.0.0.1:4000/?group=user_station");
+
+socket.on("pi_cov_from_server", (data)=>{
+  console.log(data)
+})
 
 
 function App() {
+  let [piData, setStations] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:4000/api/v1/pi-stations').then(response => response.json()).then(data => {
+    setStations([...data]);
+    });
+    return ()=>{setStations([])}
+  },[])
+
   return (
     <Router>
       <Header/>
@@ -21,7 +35,7 @@ function App() {
             <Sensor/>
           </Route>
           <Route path="/stations">
-            <Stations/>
+            <Stations piData={piData} />
           </Route>
           <Route path="/aboutpi">
             <AboutPI/>
--- react-client/src/components/stations/Stations.js
+++ react-client/src/components/stations/Stations.js
@@ -1,21 +1,10 @@
 import React , {useEffect, useState}from 'react';
 import StationsCard from "../cards/StationsCard";
-import socketIOClient from "socket.io-client";
-const socket = socketIOClient("http://127.0.0.1:4000");
 
-function Stations() {
-  let [stations, setStations] = useState([]);
-  socket.on("PI_STATION_UPDATED", (data)=>{
-    console.log(data)
-  })
-  useEffect(()=>{
-    fetch('http://localhost:4000/api/v1/pi-stations').then(response => response.json()).then(data => {
-      setStations([...data]);
-    });
-    return ()=>{setStations([])}
-  },[])
+
+function Stations({piData}) {
   return (
-    stations.map((station, key)=>{
+    piData.map((station, key)=>{
       return <StationsCard station={station} key={key}/>
      })
   );
